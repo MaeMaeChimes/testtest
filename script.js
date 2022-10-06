e4da8fe9dfab020d89e2c21dd2d504b0c8b1c459
MaeMaeChimes <108599661+MaeMaeChimes@users.noreply.github.com>
2022-10-06 15:35:49 +0800

    Add files via upload

---
+++ script.js
@@ -0,0 +1,139 @@
+var POMODORO = "POMODORO",
+  SHORT_BREAK = "SHORT_BREAK",
+  LONG_BREAK = "LONG_BREAK",
+  chime = new Audio("https://s3-us-west-2.amazonaws.com/s.cdpn.io/989813/chime.wav"),
+  progressCells = document.querySelectorAll(".progress-cell"),
+  currentInterval = null;
+
+var pTimer = {
+  mode: POMODORO,
+  duration: 0,
+  interval: null,
+  pomodoros: 0,
+  set: function() {
+    switch (this.mode) {
+      case POMODORO:
+        this.duration = 25;
+        break;
+      case SHORT_BREAK:
+        this.duration = 5;
+        break;
+      case LONG_BREAK:
+        this.duration = 15;
+        break;
+      default:
+        break;
+    }
+
+    setTimerText(this.duration, 0);
+    return this;
+  },
+  start: function() {
+    var startTime = new Date().getTime();
+
+    function timer() {
+      var elapsedSeconds = Math.floor(
+        (new Date().getTime() - startTime) / 1000
+      ),
+        remainingMinutes = this.duration - Math.ceil(elapsedSeconds / 60),
+        remainingSeconds = 60 - elapsedSeconds % 60;
+
+      setTimerText(remainingMinutes, remainingSeconds);
+
+      if (remainingMinutes <= 0 && remainingSeconds <= 60) {
+        this.stop().onEnd();
+      }
+    }
+
+    this.interval = setInterval(timer.bind(this), 200);
+
+    return this;
+  },
+  stop: function() {
+    clearInterval(this.interval);
+    this.interval = null;
+    return this;
+  },
+  reset: function() {
+    setTimerText(this.duration, 0);
+    return this;
+  },
+  onEnd: function() {
+    switch (this.mode) {
+      case POMODORO:
+        this.pomodoros++;
+        if (this.pomodoros === 4) {
+          this.mode = LONG_BREAK;
+        } else {
+          this.mode = SHORT_BREAK;
+        }
+        break;
+      case LONG_BREAK:
+        this.pomodoros = 0;
+      case SHORT_BREAK:
+        this.mode = POMODORO;
+        break;
+      default:
+        break;
+    }
+
+    chime.play();
+    updateProgressBar();
+    updateButtonText();
+  }
+};
+
+function updateProgressBar() {
+  progressCells.forEach(function(cell) {
+    cell.classList.remove("filled");
+  });
+
+  for (var i = 0; i < pTimer.pomodoros; i++) {
+    progressCells[i].classList.add("filled");
+  }
+}
+
+function setTimerText(min, sec) {
+  function timeNumToString(n) {
+    var string = n < 10 ? "0" + n : "" + n;
+
+    if (string === "60") string = "00";
+    return string;
+  }
+
+  document.getElementById("pomodoro-timer-min").innerText = timeNumToString(
+    min
+  );
+
+  document.getElementById("pomodoro-timer-sec").innerText = timeNumToString(
+    sec
+  );
+}
+
+function updateButtonText() {
+  var text = "";
+
+  if (!pTimer.interval) {
+    text += "start ";
+  } else {
+    text += "reset ";
+  }
+
+  text += pTimer.mode.split("_").join(" ");
+
+  document.getElementById("pomodoro-button").innerText = text;
+}
+
+function onButtonPress() {
+  if (!pTimer.interval) {
+    pTimer.set().start();
+  } else {
+    pTimer.stop().reset();
+  }
+
+  updateButtonText();
+}
+
+document
+  .getElementById("pomodoro-button")
+  .addEventListener("click", onButtonPress);
